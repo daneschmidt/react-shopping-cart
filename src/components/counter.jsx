194a6cdc67ccc7ec85476e0975c621bc8b961e04
Dane Schmidt <daneschmidt@gmail.com>
2020-02-08 21:01:52 -0600

    setup getBadgeClasses method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,17 +6,20 @@ class Counter extends Component {
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <div>
-        <span style={{ fontSize: 20 }} className={classes}>
+        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
