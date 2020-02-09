fa9ae27d9f177466a1e4313eba6092eec501595a
Dane Schmidt <daneschmidt@gmail.com>
2020-02-08 18:31:50 -0600

    counter setup

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World!</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
